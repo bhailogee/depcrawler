2306c819317957994a82bf4c7fd5f347334a3f11
mwasu <mwaseem@sequelmed.com>
2017-03-27 23:40:25 +0500

    App Commit

--- application/depression/www/js/myStateService.js
+++ application/depression/www/js/myStateService.js
@@ -3,28 +3,44 @@
  */
 angular.module('starter')
     .service('myStateConfig',['templates',function(templates) {
+
+
       this.myStates=[];
-      for(var i=0;i<43;i++)
+      this.statesDef=[
+          {name:"depression",count:43,view:"menuContent"},
+          {name:"anxiety",count:37,view:"menuContent3"}
+      ];
+
+
+      for(var jj=0;jj<this.statesDef.length;jj++)
       {
-        this.myStates.push({
-          "url": "/depression-"+i,
-          "name": "app.depression-"+i
-        });
+          var stateDef = this.statesDef[jj];
+          for(var i=0;i<stateDef.count;i++) {
+
+              var s =
+              {
+                  "url": "/" + stateDef.name + "-" + i,
+                  "name": "app." + stateDef.name + "-" + i
+              };
+
+              s.views=s.views||{};
+              s.views[stateDef['view']]=s.views[stateDef['view']]||{};
+
+              s.views[stateDef['view']].templateProvider = function () {
+
+                  var template=templates[s.name.split('.')[1]];
+
+                  return '<ion-view view-title="Playlist">' +
+                      '<ion-content>' +
+                      '<div class="card"><div class="item item-text-wrap">' +template+
+                      '</div></div>' +
+                      '</ion-content>' +
+                      '</ion-view>';
+              }
+              this.myStates.push(s);
+          }
       }
-        this.myStates.forEach(function (s) {
-            s.views=s.views||{};
-            s.views.menuContent=s.views.menuContent||{};
 
-            s.views.menuContent.templateProvider = function () {
 
-                var template=templates[s.name.split('.')[1]];
 
-                return '<ion-view view-title="Playlist">' +
-                '<ion-content>' +
-                '<div class="card"><div class="item item-text-wrap">' +template+
-                '</div></div>' +
-                '</ion-content>' +
-                '</ion-view>';
-            }
-        })
     }]);
